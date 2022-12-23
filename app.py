37145d95a4bad7d4e3d5a430f46b40a1d941efbc
F74092057 <iamagod76@gmail.com>
2022-12-23 14:24:22 +0800

    1223 new

--- app.py
+++ app.py
@@ -14,16 +14,12 @@ line_bot_api = LineBotApi('Dv/jLTe9Hjfv29yLkOKmE3NsPmGQQhPP6Hj+CfzoN716mEEpOokXf
 
 handler = WebhookHandler('641700cfc7bd588430948046b6c3c9a8')
 
-line_bot_api.push_message('U0b55f1fefdcf18168b0c8c515701a585', TextSendMessage(text='ä½ å¯ä»¥é–‹å§‹äº†'))
-
-
 
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
- 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
@@ -33,14 +29,19 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
+
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
+    if (message.find("§Ú¬O©÷à±", beg=0, end=len(message)!= -1)) :
+        message = "¨ş¨ş¨ş"
     line_bot_api.reply_message(event.reply_token,message)
+    
+
+    
+
 
 import os
 if __name__ == "__main__":
